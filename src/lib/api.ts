1ddc7866e8d08d4337185ba7fbbc695edc208bac
zsfVishnu <vishnu.tp32@gmail.com>
2025-08-11 11:57:17 +0530

    feat: hc menu

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import { MenuResponse } from '@/types/menu';
 
 export async function getMenu(): Promise<MenuResponse> {
-  const response = await fetch('http://localhost:8080/menu');
+  // const response = await fetch('http://localhost:8080/menu');
+  const response = await fetch('/api/menu');
   
   if (!response.ok) {
     throw new Error('Failed to fetch menu');
